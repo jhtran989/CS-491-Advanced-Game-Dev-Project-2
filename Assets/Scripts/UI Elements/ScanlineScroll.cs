76abc1c4734fc83bd939ac79db66e215cf04e2a2
Andrew Valdez <avz@unm.edu>
2023-03-26 20:09:54 -0600

    small changes and additions for terminal

--- Assets/Scripts/UI Elements/ScanlineScroll.cs
+++ Assets/Scripts/UI Elements/ScanlineScroll.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ScanlineScroll : MonoBehaviour
 {
-    public RawImage img;
-    public float x, y;
+    private MeshRenderer meshRenderer;
+    [SerializeField] private float speed;
 
-    private void FixedUpdate() {
-        img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(0, speed * Time.deltaTime);
     }
 }
