e369b53684e9934c642f23af02de03fdafc7a55e
John Tran <jtran989@unm.edu>
2023-04-21 21:07:37 -0600

    Fixed upside down inverted controls

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     public float xSensitivity, ySensitivity;
-    float xRotation, yRotation;
+    float xRotation, yRotation, zRotation;
     public Transform orientation;
     private bool allowedToLook = true;
     
@@ -47,9 +47,27 @@ public class PlayerCamera : MonoBehaviour
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * xSensitivity;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * ySensitivity;
 
+        // add flipped controls if upside down (only side by side motion controlled by yRotation)
+        if (IsUpsideDown())
+        {
+            yRotation -= mouseX; 
+            xRotation -= mouseY;
+            zRotation = transform.rotation.z;
+            
+            // Debug.Log("Upside Down...");
+        }
+        else
+        {
+            yRotation += mouseX; 
+            xRotation -= mouseY;
+            zRotation = transform.rotation.z;
+            
+            // Debug.Log("Upside Up...");
+        }
         
-        yRotation += mouseX; 
-        xRotation -= mouseY;
+        // yRotation += mouseX; 
+        // xRotation -= mouseY;
+        // zRotation = transform.rotation.z;
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(xRotation, yRotation, 0);
@@ -57,6 +75,31 @@ public class PlayerCamera : MonoBehaviour
 
     public void AllowLook(bool canLook) {
         allowedToLook = canLook;
+    }
+
+    private bool IsUpsideDown()
+    {
+        // FIXME: z rotation doesn't matter (changes in inspector 180/-180, but actual value does not change)
+        // need to take the mod (%) of 360 (actual value keeps accumulating past 360 and -360)
+        // between 90 and 270 or -90 and -270 (can just take abs) -- symmetric if divisor is positive (360 for our case)
+        // 5 % 4 = 1
+        // -5 % 4 = -1
+        // Debug.Log("x rotation = " + xRotation);
+        // Debug.Log("z rotation = " + zRotation);
+        
+        // if xRotation is >90 or <-90 OR if z-rotation is 180/-180 (just through observations)
+        // just based on transform (don't check orientation)
+        // need xRotation because flip is along x-direction, not yRotation
+        var xRotationStandard = Mathf.Abs(xRotation % 360);
+        
+        // Debug.Log("x rotation standard = " + xRotationStandard);
+        
+        if (xRotationStandard is > 90 and < 270)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     public void EnableCamera()
