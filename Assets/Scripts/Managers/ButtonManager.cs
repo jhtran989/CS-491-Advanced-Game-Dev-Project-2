60121fd268c61b25a3a07f14d9f711cdd7a8d5f9
Andrew Valdez <avz@unm.edu>
2023-04-25 16:32:56 -0600

    fixed menu screens and added pause menu (time persists)

--- Assets/Scripts/Managers/ButtonManager.cs
+++ Assets/Scripts/Managers/ButtonManager.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour {
 
+    public GameObject HUD, PauseScreen;
+    private bool isPaused;
+
     private GameManager gm;
 
     private void Start() {
         gm = GameManager.instance;
+        isPaused = false;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown("Escape")) {
+            if (isPaused) ResumeButton();
+            else PauseButton();
+        }
     }
 
     public void PlayButton() {
@@ -16,6 +27,22 @@ public class ButtonManager : MonoBehaviour {
 
     public void QuitButton() {
         gm.QuitGame();
+    }
+
+    public void ResumeButton() {
+        HUD.GetComponent<Renderer>().enabled = true;
+        PauseScreen.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;  
+        isPaused = false;
+    }
+
+    public void PauseButton() {
+        HUD.GetComponent<Renderer>().enabled = false;
+        PauseScreen.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;  
+        isPaused = true;
     }
 
 }
